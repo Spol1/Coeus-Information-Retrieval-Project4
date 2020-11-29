8cf0c7fcc700baf236bb8bd0cf427a8d0e2e3582
yohanthshetty <yohanth.shetty@ves.ac.in>
2020-11-29 16:41:49 +0530

    Proj structure created

---
+++ coecus_search/app/__init__.py
@@ -0,0 +1,17 @@
+from flask import Flask, render_template, request
+import urllib.request
+import plotly
+import plotly.graph_objects as go
+import pandas as pd
+import pymongo
+from pymongo import MongoClient
+from pymongo.errors import ServerSelectionTimeoutError
+import json
+
+app = Flask(__name__)
+
+client = MongoClient("mongodb://localhost:27017/")
+db = client["tweetsDB"]
+
+from app import views
+
